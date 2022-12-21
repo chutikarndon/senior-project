8d97c77a76c2ad111db56392558ada1b227a5409
ammpiywn <piyawan9239@hotmail.co.th>
2022-12-21 13:23:15 +0700

    21/12

--- src/Page/RoomMeet.js
+++ src/Page/RoomMeet.js
@@ -2,8 +2,8 @@ import React, {useState} from "react";
 import DragMove from "../DragMove";
 // import { Animate } from "react-simple-animate";
 // import logo from "../logo.svg";
-import { Button, makeStyles } from "@material-ui/core";
-import {AccountCircleOutlined, ChatOutlined, KeyboardVoiceOutlined, CallEndOutlined, VideocamOutlined, SettingsOutlined, WhatshotOutlined} from '@material-ui/icons';
+import { Button, makeStyles} from "@material-ui/core";
+import {AccountCircleOutlined, ChatOutlined, KeyboardVoiceOutlined, CallEndOutlined, VideocamOutlined, SettingsOutlined, WhatshotOutlined, HomeOutlined} from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
    button: {
@@ -14,6 +14,7 @@ const useStyles = makeStyles((theme) => ({
 
 const RoomMeet = () => {
     const classes = useStyles();
+    const [showModal, setShowModal] = useState(false);
     // const state = {play: false};
     const [translate, setTranslate] = useState({
         x: 0,
@@ -29,10 +30,66 @@ const RoomMeet = () => {
       
     return(
         <div className="flex flex-col">            
-            <div className="flex flex-row justify-between mt-4 ml-6 mr-4">  {/* Header*/}
-                <div className="flex-none w-15 h-15">     {/* sidebar*/}
-                    home
-                </div>    
+            <div className="flex flex-row justify-between mt-4 ml-6 mr-4">  {/* Header*/}    
+                <div > {/* sidebar*/}
+                    <Button
+                        onClick={() => setShowModal(true)}
+                        class="overflow-hidden rounded-full w-16 h-10 flex justify-center items-center hover:cursor-pointer" startIcon={<HomeOutlined fontSize="small"/>}>     
+                    </Button>
+                    {showModal?(
+                        <>
+                            <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none w-8/12 h-5/6 mt-8 ml-32">
+                                <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                                {/*content*/}
+                                    <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                        {/*header*/}
+                                        <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                                            <h3 className="text-3xl font-semibold">
+                                                Modal Title
+                                            </h3>
+                                            <button
+                                                className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                                onClick={() => setShowModal(false)}
+                                            >
+                                                <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                                ×
+                                                </span>
+                                            </button>
+                                        </div>
+                                        {/*body*/}
+                                        <div className="relative p-6 flex-auto">
+                                            <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                                                I always felt like I could do anything. That’s the main
+                                                thing people are controlled by! Thoughts- their perception
+                                                of themselves! They're slowed down by their perception of
+                                                themselves. If you're taught you can’t do anything, you
+                                                won’t do anything. I was taught I could do everything.
+                                            </p>
+                                        </div>
+                                        {/*footer*/}
+                                        <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                                            <button
+                                                className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                                type="button"
+                                                onClick={() => setShowModal(false)}
+                                            >
+                                                Close
+                                            </button>
+                                            <button
+                                                className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                                type="button"
+                                                onClick={() => setShowModal(false)}
+                                            >
+                                                Save Changes
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                        </>    
+                    ): null}
+                </div>   
                 <div className=" overflow-x-hidden">      {/* other user*/}
                     other users video
                     <div className=" grid gap-2 grid-cols-none"></div>
@@ -45,7 +102,7 @@ const RoomMeet = () => {
             <div className="flex justify-between items-end ml-4 mr-2 mt-6">   {/* toolbar/ video me*/}
                 <div className="rounded-full box-content h-7 w-56 border-0 shadow-md bg-red-200 ">  {/* toolbar*/}
                     <div className=" flex">           
-                        <Button class=" flex-none w-8" className={classes.button} startIcon={<WhatshotOutlined fontSize="small"/>}></Button>
+                        <Button class=" flex-none w-8" className={classes.button} startIcon={<WhatshotOutlined fontSize="small"/>} ></Button>
                         <Button class=" flex-none w-8" className={classes.button} startIcon={<SettingsOutlined fontSize="small"/>}></Button>
                         <Button class=" flex-none w-8" className={classes.button} startIcon={<VideocamOutlined fontSize="small"/>}></Button>
                         <Button class=" flex-none w-8" className={classes.button} startIcon={<CallEndOutlined fontSize="small"/>}></Button>
